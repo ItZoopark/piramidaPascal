b0fad76f6a8e287c8b9471bcc3e99340beb682ee
KruzhokProg <Aa1@3$5^7>
2021-09-12 15:55:16 +0300

    done

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ for k in range(0, n):
         count += 1
     last_row_count += count
 
-print(f"last row count: {last_row_count + n -1 }")
+# print(f"last row count: {last_row_count + n - 1}")
 
 for i in range(N):
 
@@ -52,40 +52,40 @@ for i in range(N):
             Cnk //= 10
             count += 1
         cur_row_count += count
-    print(f"длина текущей строки {i}: {cur_row_count + i}")
+    # print(f"длина текущей строки {i}: {cur_row_count + i}")
 
-    # вычисляем длину следующей строки
-    i_next = i + 1
-    next_row_count = 0
-    fn = 1
-    for i_n in range(2, i_next+1):
-        fn *= i_n
-
-    for k in range(0, i_next+1):
-        count = 0
-
-        fk = 1
-        for i_k in range(2, k + 1):
-            fk *= i_k
-
-        fnk = 1
-        for i_nk in range(2, i_next - k + 1):
-            fnk *= i_nk
-
-        Cnk = fn // (fk * fnk)
-
-        while Cnk != 0:
-            Cnk //= 10
-            count += 1
-        next_row_count += count
-    print(f"длина следующей строки {i}: {next_row_count + i_next}")
+    # # вычисляем длину следующей строки
+    # i_next = i + 1
+    # next_row_count = 0
+    # fn = 1
+    # for i_n in range(2, i_next+1):
+    #     fn *= i_n
+    #
+    # for k in range(0, i_next+1):
+    #     count = 0
+    #
+    #     fk = 1
+    #     for i_k in range(2, k + 1):
+    #         fk *= i_k
+    #
+    #     fnk = 1
+    #     for i_nk in range(2, i_next - k + 1):
+    #         fnk *= i_nk
+    #
+    #     Cnk = fn // (fk * fnk)
+    #
+    #     while Cnk != 0:
+    #         Cnk //= 10
+    #         count += 1
+    #     next_row_count += count
+    # print(f"длина следующей строки {i}: {next_row_count + i_next}")
 
     # ------------------------------
     # выводим пробелы
-    count_spaces = (next_row_count + i_next) - (cur_row_count + i)
-    print(f"count spaces: {count_spaces}")
-    # for i in range(count_spaces//2):
-    #     print(" ")
+    count_spaces = (last_row_count + n - 1) - (cur_row_count + i)
+    # print(f"count spaces: {count_spaces}")
+    for l in range(count_spaces//2):
+        print(" ", end ="")
 
     for k in range(0, i + 1):
         fi = 1
